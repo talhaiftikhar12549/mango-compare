a7aa2b347a51a00e29d246415fe522eda88bfae6
Ali AHMAD <ali.ali.ahmad22@gmail.com>
2025-03-25 13:04:26 +0500

    installed react js with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-blue-500 text-white py-10 text-center text-3xl'>lets get things started</div>
+  )
+}
+
+export default App
