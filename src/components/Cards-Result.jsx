0ca2a173a63d9e3a527e2c8085c65ca4b0df167a
Talha <talhaiftikhar12549@gmail.com>
2025-04-23 22:00:21 +0500

    added email check to verify if the user is logged in or not and saved email in local storage

--- src/components/Cards-Result.jsx
+++ src/components/Cards-Result.jsx
@@ -1,16 +1,42 @@
 import Img from "../assets/price tool/img.png";
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+import { IoClose } from "react-icons/io5";
 export default function CardResult({ sortedPrice, sortedRating }) {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("userEmail", email);
+    localStorage.setItem("fullName", fullName);
+    console.log("Saved to localStorage:", { fullName, email });
+    if (email !== "") {
+      setIsSubscribed(true);
+    }
+    closeModal();
+  };
+  useEffect(() => {
+    const storedEmail = localStorage.getItem("userEmail");
+    if (storedEmail) {
+      setIsSubscribed(true);
+    }
+  }, []);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 4;
   const totalPages = Math.ceil(sortedPrice.length / itemsPerPage);
-  
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const pageData = useMemo(() => {
     const start = (currentPage - 1) * itemsPerPage;
-    return sortedPrice.slice(start, start + itemsPerPage); 
-  } , [sortedPrice, sortedRating, currentPage, itemsPerPage]);
-
+    return sortedPrice.slice(start, start + itemsPerPage);
+  }, [sortedPrice, sortedRating, currentPage, itemsPerPage]);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const toggelModal = () => {
+    setIsModalOpen(true);
+  }
+ 
   return (
     <>
       {pageData.map(srtdata => (
@@ -28,7 +54,7 @@ export default function CardResult({ sortedPrice, sortedRating }) {
             <p>{srtdata.dosage}</p>
           </div>
           <div className="w-[15%] flex items-center justify-center">
-            <p>£ {srtdata.price}</p>
+            {isSubscribed ? <p>£ {srtdata.price}</p> : <p onClick={toggelModal} className="text-[#FCC821] text-[18px] font-[600] cursor-pointer border-b-[2px] border-[#ffffff] hover:border-b-[2px] hover:border-[#FCC821] transition duration-500">View Price</p>}
           </div>
           <div className="w-[15%] flex items-center justify-center">
             <p>{srtdata.quantity}</p>
@@ -76,6 +102,58 @@ export default function CardResult({ sortedPrice, sortedRating }) {
           Next
         </button>
       </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm transition duration-300">
+          <div className="bg-white rounded-lg shadow-lg p-6 max-w-sm w-full transform scale-100 transition duration-300">
+
+            <div className="relative flex items-center justify-between mb-4">
+              <h2 className="text-[24px] border-b border-[#E4E4E4] pb-[10px] font-[700] text-[#070707] mb-0 text-center w-full">
+                ADD DETAIL
+              </h2>
+              <div onClick={closeModal} className="absolute text-[30px] flex align-items-center right-0 top-0 cursor-pointer">
+                <IoClose />
+              </div>
+            </div>
+
+
+
+            {/* Form Starts Here */}
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">Full Name</label>
+                <input
+                  type="text"
+                  required
+                  placeholder="Jhon"
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                  className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
+                <input
+                  type="email"
+                  required
+                  placeholder="example@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full cursor-pointer transition duration-700 mt-2 px-4 py-2 bg-[#FCC821] text-white rounded hover:text-[#FCC821] hover:bg-white border-2 border-[#FCC821]"
+              >
+                Save
+              </button>
+            </form>
+            {/* Form Ends Here */}
+          </div>
+        </div>
+      )}
+
     </>
   );
 }
