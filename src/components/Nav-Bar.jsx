42d6c55a0016605e117e76ee4e5e1c131e1a47a0
Talha <talhaiftikhar12549@gmail.com>
2025-04-26 13:55:49 +0500

    added responsiveness to the navbar in the

--- src/components/Nav-Bar.jsx
+++ src/components/Nav-Bar.jsx
@@ -1,25 +1,23 @@
 import logo from "../assets/nav/logo.png";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 export default function NavBar() {
-  // const options = [
-  //   { label: "Home", path: "/" },
-  //   { label: "Menu", path: "/menu" },
-  //   { label: "About Us", path: "/aboutus" },
-  // ];
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log("aaaa");
+  };
   return (
     <>
-      <header className=" w-full max-w-[1280px] pt-[32px] pb-[34px]">
+      <header className=" w-full max-w-[1280px] lg:px-[40px] xl:px-0 px-[10px] mx-auto pt-[32px] pb-[34px]">
         <div className="flex justify-between items-center max-w-[1280px]">
-          <div className="w-[40%]">
-            <NavLink
-              to="/"
-              className="pointer"
-              
-            >
+          <div className="w-[50%] md:w-[30%] lg:w-[40%] ">
+            <NavLink to="/" className="pointer">
               <img src={logo} alt="" />
             </NavLink>
           </div>
-          <div className="flex justify-between items-center w-[60%]">
+          <div className="hidden md:flex justify-between items-center w-[65%] lg:w-[60%]">
             <div>
               <ul className="flex gap-[30px] text-[16px] font-[500] font-montserrat text-[#222222]">
                 <li>
@@ -78,13 +76,67 @@ export default function NavBar() {
                 className={({ isActive }) =>
                   isActive
                     ? "text-[#FFFFFF] rounded-[10px] bg-[#FCC821] border border-[#FCC821] transition duration-700 font-bold  py-[15px] px-[30px]"
-                    : "text-[#202244] rounded-[10px] bg-[#FFFFFF] border border-[#202244] transition duration-700 font-bold py-[15px] px-[30px] "
+                    : "text-[#202244] rounded-[10px] bg-[#FFFFFF] border border-[#202244] transition duration-700 font-bold py-[15px] px-[15px] lg:px-[30px] "
                 }
               >
                 <button className=" cursor-pointer  rounded-[10px]">
                   Contant Now
                 </button>
               </NavLink>
+            </div>
+          </div>
+
+          <div className="md:hidden flex">
+            <div>
+              {" "}
+              <div
+                onClick={toggleMenu}
+                className="bg-[#FCC821] h-[40px] w-[40px] flex flex-col justify-center items-center cursor-pointer rounded"
+              >
+                <div className="px-3 my-0.5 py-[1px]  bg-[#ffffff]"></div>
+                <div className="px-3 my-0.5 py-[1px]  bg-[#ffffff]"></div>
+                <div className="px-3 my-0.5 py-[1px]  bg-[#ffffff]"></div>
+              </div>
+              {isOpen && (
+                <ul className="absolute right-3 top-20  border bg-[#ffffff] border-[#FCC821] rounded-md shadow-xl mt-2 w-48 z-10">
+                  {/* {options.map((option, index) => (
+                    <li
+                      key={index}
+                      className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                      onClick={() => {
+                        option.value();
+                        setIsOpen(false);
+                      }}
+                    >
+                      {option.label}
+                    </li>
+                  ))} */}
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-[#222222] font-bold text-center">
+                    <NavLink className={"block w-[100%]"} to="/">Home</NavLink>
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-[#222222] font-bold text-center">
+                  <NavLink className={"block w-[100%]"} to="/mounjaro-compare">Mounjaro</NavLink>
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-[#222222] font-bold text-center">
+                    <NavLink className={"block w-[100%]"} to="/wegovy-compare">Wegovy</NavLink>
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-[#222222] font-bold text-center">
+                    <NavLink className={"block w-[100%]"} to="/blogs">Blogs</NavLink>
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-[#222222] font-bold text-center">
+  <NavLink
+    to="/contact-us"
+    className={({ isActive }) =>
+      isActive
+        ? "block w-full text-[#FFFFFF] rounded-[10px] bg-[#FCC821] border border-[#FCC821] transition duration-700 font-bold py-[10px] px-[15px]"
+        : "block w-full text-[#202244] rounded-[10px] bg-[#FFFFFF] border border-[#202244] transition duration-700 font-bold py-[10px] px-[15px] lg:px-[30px]"
+    }
+  >
+    Contact Us
+  </NavLink>
+</li>
+                </ul>
+              )}
             </div>
           </div>
         </div>
