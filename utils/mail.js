49a4423bfa246f471fc083988e3a6b72d0e22653
Wali Ahmed <102593062+AliAhmadAx@users.noreply.github.com>
2025-05-13 12:19:02 +0500

    mail.js added

---
+++ utils/mail.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail', // or another email service
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendMail = async (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+module.exports = sendMail;
