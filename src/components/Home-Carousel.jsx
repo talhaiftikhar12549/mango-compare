4abb3a01cecfb406a9a8cc6838721a2f29c0b3d4
Talha <talhaiftikhar12549@gmail.com>
2025-05-20 12:53:03 +0500

    Restyled home carousal

--- src/components/Home-Carousel.jsx
+++ src/components/Home-Carousel.jsx
@@ -1,10 +1,13 @@
-import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import img1 from "../assets/home/img1.png";
+import img2 from "../assets/home/img2.png";
+import img3 from "../assets/home/img3.png";
+import img4 from "../assets/home/img4.png";
 
 const Carousel = () => {
-   const settings = {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
@@ -14,26 +17,49 @@ const Carousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: true,
-    centerPadding: "0px", // Center properly on all screens
+    centerPadding: "0px",
   };
+
+  const slides = [
+    {
+      text: "Wondering how much Mounjaro costs in the UK?",
+      image: img1,
+    },
+    {
+      text: "Not sure where to buy Wegovy at the cheapest price in the UK?",
+      image: img2,
+    },
+    {
+      text: "Mango is here to end the Mounjaro vs. Wegovy price debate with…",
+      image: img3,
+    },
+    {
+      text: "Effortless price comparisons for weight loss meds in the UK!",
+      image: img4,
+    },
+  ];
 
   return (
     <div className="w-[95%] max-w-[1280px] mx-auto py-10">
       <Slider {...settings}>
-        {[
-          "Wondering how much Mounjaro costs in the UK?",
-          "Not sure where to buy Wegovy at the cheapest price in the UK?",
-          "Mango is here to end the Mounjaro vs. Wegovy price debate with…",
-          "Effortless price comparisons for weight loss meds in the UK!",
-        ].map((text, index) => (
+        {slides.map((slide, index) => (
           <div
             key={index}
-            className="flex justify-center items-center  px-4 sm:px-8 md:px-16"
+            className="flex justify-center items-center px-4 sm:px-8 md:px-16"
           >
             <div className="w-full">
-              <div className="bg-[#FF9E34] p-6 sm:p-10 md:p-16 md:min-h-[317px] rounded-[20px] min-h-[216px] sm:min-h-[242px] lg:min-h-[300px] xl:min-h-[416px]">
-                <h2 className="text-white font-[900] text-[28px] sm:text-[36px] md:text-[42px] xl:text-[64px] font-montserrat text-center">
-                  {text}
+              <div
+                className="relative rounded-[20px] overflow-hidden min-h-[216px] sm:min-h-[242px] lg:min-h-[300px] xl:min-h-[416px] bg-cover bg-center flex items-center justify-center"
+                style={{
+                  backgroundImage: `url(${slide.image})`,
+                }}
+              >
+                {/* Orange overlay */}
+                <div className="absolute inset-0 opacity-70"></div>
+
+                {/* Text */}
+                <h2 className="relative z-10 text-white font-[900] text-[28px] sm:text-[36px] md:text-[42px] xl:text-[48px] font-montserrat text-center px-4">
+                  {slide.text}
                 </h2>
               </div>
             </div>
@@ -42,6 +68,6 @@ const Carousel = () => {
       </Slider>
     </div>
   );
-};  
+};
 
 export default Carousel;
