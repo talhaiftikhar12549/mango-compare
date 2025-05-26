a774400f4fef61466c35852d083b858c1a9cfe0d
Ali AHMAD <102593062+AliAhmadAx@users.noreply.github.com>
2025-05-26 12:16:28 +0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,7 +61,7 @@ exports.login = async (req, res, next) => {
 exports.getMe = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id);
-
+    
     res.status(200).json({
       success: true,
       data: user
@@ -93,6 +93,7 @@ const sendTokenResponse = (user, statusCode, res) => {
   
   res.status(statusCode).json({
     success: true,
+    role: user.role,
     token // Send the token in the response body
   });
 };
